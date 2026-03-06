669dae2bef7414403ed8b750a815683914131e34
ywan6541 <3568993916@qq.com>
2026-03-06 03:23:49 +0000

    1

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["get","post"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["get","post"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/ethics",methods=["get","post"])
+def ethics():
+    return(render_template("ethics.html"))
+
+@app.route("/correct",methods=["get","post"])
+def correct():
+    return(render_template("correct.html"))
+
+@app.route("/wrong",methods=["get","post"])
+def wrong():
+    return(render_template("wrong.html"))
+
+if __name__ == "__main__":
+    app.run()
